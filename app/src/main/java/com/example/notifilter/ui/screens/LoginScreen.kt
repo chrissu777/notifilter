61a8f235a3014513f74e8ce3c726dcee248ca60f
chrissu777 <49414356+chrissu777@users.noreply.github.com>
2024-12-24 03:06:10 -0800

    initial commit

---
+++ app/src/main/java/com/example/notifilter/ui/screens/LoginScreen.kt
@@ -0,0 +1,30 @@
+package com.example.notifilter.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.notifilter.Greeting
+import com.example.notifilter.ui.theme.NotifilterTheme
+
+@Composable
+fun LoginScreen(){
+    Column(){
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LoginScreen()
+}
---
+++ app/src/main/java/com/example/notifilter/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.notifilter.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0x333A3F  )
+val DarkGray = Color(0x4C4C4D)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
