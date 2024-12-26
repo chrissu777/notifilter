531c2dbea0b29a1f6fbf6641d5bb729014a9ff1b
chrissu777 <49414356+chrissu777@users.noreply.github.com>
2024-12-25 22:00:22 -0800

    home screen placeholder file

--- app/src/main/java/com/example/notifilter/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/notifilter/ui/screens/HomeScreen.kt
@@ -1,2 +1,33 @@
 package com.example.notifilter.ui.screens
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(textAlign = TextAlign.Center, text = "Placeholder")
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    HomeScreen()
+}
+
